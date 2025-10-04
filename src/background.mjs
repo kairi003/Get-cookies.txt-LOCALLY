fa4ea2d2b0910245657b2f9ca86290aa09e9c798
Ryusuke Nagai <rnagai@lycorp.co.jp>
2025-10-04 18:52:45 +0900

    Fix: handle undefined message data in onMessage listener

--- src/background.mjs
+++ src/background.mjs
@@ -64,10 +64,10 @@ chrome.notifications.onButtonClicked.addListener(
 // TODO: use offscreen API to integrate implementation in chrome and firefox
 // Save file message listener for firefox
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  const { type, target, data } = message;
+  const { type, target, data } = message || {};
   if (target !== 'background') return;
   if (type === 'save') {
-    const { text, name, format, saveAs } = data;
+    const { text, name, format, saveAs } = data || {};
     await saveToFile(text, name, format, saveAs);
     sendResponse('done');
     return;
