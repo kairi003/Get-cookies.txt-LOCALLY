85c3e07ac77150d6b03c132d382fb84c85630880
kairi <kairi.satellite@gmail.com>
2025-10-06 03:06:23 +0900

    Update to remember nowrap option's state (#93)

--- src/table-nowrap.js
+++ src/table-nowrap.js
@@ -1,3 +1,14 @@
-document.querySelector('#nowrapOption').addEventListener('change', (e) => {
-  document.querySelector('#netscapeTable').classList.toggle('nowrap');
-});
+{
+  const nowrapOption = document.querySelector('#nowrapOption');
+  const tableElement = document.querySelector('#netscapeTable');
+
+  const selectedFormat =
+    (localStorage.getItem('tableNowrap') || 'true') === 'true';
+  nowrapOption.checked = selectedFormat;
+  tableElement.classList.toggle('nowrap', selectedFormat);
+
+  nowrapOption.addEventListener('change', (e) => {
+    tableElement.classList.toggle('nowrap');
+    localStorage.setItem('tableNowrap', e.target.checked);
+  });
+}
